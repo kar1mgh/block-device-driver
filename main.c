94da820567bda7fa73daa0255e37d7652c9b4378
Karim Shakirov <karimshak06@gmail.com>
2024-07-15 12:29:45 +0300

    fix: compilation errors

--- main.c
+++ main.c
@@ -23,6 +23,28 @@ static void __exit blkdev_driver_exit(void)
 	pr_info("driver exit\n");
 }
 
+static int open_bdev(void)
+{
+	struct file *bdev_file = bdev_file_open_by_path(blkdev_name, BLK_OPEN_READ | BLK_OPEN_WRITE, NULL, NULL);
+	
+	if (IS_ERR(bdev_file))
+		return PTR_ERR(bdev_file);
+
+	blkdev = file_bdev(bdev_file);
+
+	return 0;
+}
+
+static int close_bdev(void)
+{
+	if (!blkdev)
+		return -EINVAL;
+	
+	kfree(blkdev);
+	
+	return 0;
+}
+
 static int driver_module_execute_command_set(const char *arg, const struct kernel_param *kp)
 {
 	if (!blkdev_name)
@@ -30,41 +52,17 @@ static int driver_module_execute_command_set(const char *arg, const struct kerne
 
 	int error = 0;
 
-	if (!strcmp(command, "open")) { 
+	if (!strcmp(arg, "open")) { 
 		error = open_bdev();
-	} else if (!strcmp(command, "close")) {
+	} else if (!strcmp(arg, "close")) {
 		error = close_bdev();
-	} else if (!strcmp(command, "get name")) {
-		if (!blkdev):
+	} else if (!strcmp(arg, "get name")) {
+		if (!blkdev)
 			return -EINVAL;
 
-		pr_warn("%s\n", device_name) // should work only if device is openned
+		pr_warn("%s\n", blkdev_name); // should work only if device is openned
 	} else return -EINVAL;
     
-	return 0;
-}
-
-static int open_bdev()
-{
-	struct file *bdev_file = bdev_file_open_by_path(blkdev_name, BLK_OPEN_READ | BLK_OPEN_WRITE, NULL, NULL);
-	
-	if (IS_ERR(bdev_file)) {
-		error = PTR_ERR(bdev_file);
-		return error;
-	}
-
-	blkdev = *file_bdev(file);
-
-	return 0;
-}
-
-static int close_bdev()
-{
-	if (!blkdev)
-		return -EINVAL;
-	
-	kfree(blkdev);
-	
 	return 0;
 }
 
@@ -76,11 +74,11 @@ static const struct kernel_param_ops driver_module_execute_command_ops = {
 MODULE_PARM_DESC(execute_command, "Execute_command");
 module_param_cb(execute_command, &driver_module_execute_command_ops, NULL, S_IWUSR);
 
-MODULE_PARM_DESC(device_name, "Device name");
-module_param_named(device_name, blkdev_name, charp, S_IRUGO | S_IWUSR);
+MODULE_PARM_DESC(blkdev_name, "Block device name");
+module_param_named(blkdev_name, blkdev_name, charp, S_IRUGO | S_IWUSR);
 
-module_init(driver_init);
-module_exit(driver_exit);
+module_init(blkdev_driver_init);
+module_exit(blkdev_driver_exit);
 
 MODULE_AUTHOR("Karim Shakirov <karimshak06@gmail.com>");
 MODULE_LICENSE("GPL");
